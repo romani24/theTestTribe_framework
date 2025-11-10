9597420bb6d10fe8e694423484542266d3b26672
romani24 <raromani24@gmail.com>
2025-11-10 17:53:49 +0000

    init

--- TheTestTribe_Playwright/tests/secondTest.spec.ts
+++
@@ -1,8 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('My Second Test', async ({page}) => {
-    await page.goto('https://google.com');
-    
-    //Expect a title
-    await expect(page).toHaveTitle('Google');
-});
--- playwright_framework/playwright.config.ts
+++ playwright_framework/playwright.config.ts
@@ -29,7 +29,6 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
     trace: 'on-first-retry',
   },
 
