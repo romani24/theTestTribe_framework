57d01867feffea57c19e1bfed004050f136152d3
romani24 <raromani24@gmail.com>
2025-11-19 22:26:31 +0000

    init

--- playwright_framework/tests/groupTest.spec.ts
+++ playwright_framework/tests/groupTest.spec.ts
@@ -1,27 +1,31 @@
-import { test,expect, Page } from '@playwright/test';     
+import { test, expect, Page } from '@playwright/test';
 
-test.beforeAll(async()=>{
+test.describe('Suite', () => {
+    test('Test 1', async () => {
+        console.log("1");
+    })
+    test('Test 2', async () => {
+        console.log("2");
+    })
+})
+
+test.beforeAll(async () => {
     console.log("DB conn setup");
-}) 
+})
 
-test.beforeEach(async({page})=>{
+test.beforeEach(async ({ page }) => {
     console.log("Clearing cookies");
-})   
+})
 
-test.afterEach(async()=>{
+test.afterEach(async () => {
     console.log("Cache Removal");
-})        
+})
 
-test.afterAll(async()=>{
+test.afterAll(async () => {
     console.log("DB conn disconnect");
 })
 
-test('Test 1',async()=>{
-    console.log("1");
-})
-test('Test 2',async()=>{
-    console.log("2");
-})
 
 
-  
+
+
